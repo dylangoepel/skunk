f3ab0b31b2fe1d1c7f7e3042ef921bb8cd3687cf
Dylan Göpel <dylan.connor@t-online.de>
2022-11-27 16:11:13 +0100

    add prototype main

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import System.Environment (getArgs)
+import Eval (eval)
+import Syntax (filep, fparse)
+
 main :: IO ()
-main = fail "not implemented."
+main = do
+    args <- getArgs
+    txts <- sequence $ map readFile args
+    res <- fparse filep $ concat txts
+    case eval res of
+        Nothing -> fail "unable to eval."
+        Just r -> putStr r
